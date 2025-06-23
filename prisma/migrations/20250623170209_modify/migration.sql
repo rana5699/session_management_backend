9142699c21d97041ec0dfc37c453755b577a76a7
sohel rana <ranaot56@gmail.com>
2025-06-23 23:02:49 +0600

    ♻️ refactor(schema): revamp database schema with new models

---
+++ prisma/migrations/20250623170209_modify/migration.sql
@@ -0,0 +1,362 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('PATIENT', 'THERAPIST', 'DOCTOR', 'ADMIN', 'SUPERADMIN');
+
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE', 'BOY', 'GIRL', 'OTHER');
+
+-- CreateEnum
+CREATE TYPE "AppointmentStatus" AS ENUM ('PENDING', 'BOOKED', 'COMPLETED', 'RUNNING', 'MISSED', 'CANCELLED');
+
+-- CreateEnum
+CREATE TYPE "DepartmentType" AS ENUM ('MEDICAL', 'OCCUPATIONAL', 'PSYCHOLOGICAL', 'PHYSIO', 'SPEECH_LANGUAGE', 'ADMINISTRATION', 'OTHER');
+
+-- CreateEnum
+CREATE TYPE "PaymentStatus" AS ENUM ('PENDING', 'PAID', 'FAILED', 'CANCELLED');
+
+-- CreateEnum
+CREATE TYPE "PaymentMethod" AS ENUM ('NAGAD', 'BKASH', 'CARD', 'CASH');
+
+-- CreateEnum
+CREATE TYPE "LanguagePreference" AS ENUM ('BANGLA', 'ENGLISH');
+
+-- CreateEnum
+CREATE TYPE "UploadedType" AS ENUM ('REPORT', 'PRESCRIPTION', 'OTHER');
+
+-- CreateEnum
+CREATE TYPE "RecordsType" AS ENUM ('REPORT', 'PRESCRIPTION', 'OTHER');
+
+-- CreateEnum
+CREATE TYPE "UploadedBy" AS ENUM ('PATIENT', 'THERAPIST', 'DOCTOR', 'ADMIN', 'OTHER');
+
+-- CreateEnum
+CREATE TYPE "ScheduleAvailabilityType" AS ENUM ('WEEKLY', 'MONTHLY', 'ONE_TIME', 'RANGE');
+
+-- CreateEnum
+CREATE TYPE "AppointmentType" AS ENUM ('ONE_TO_ONE', 'GROUP');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "userKey" TEXT NOT NULL,
+    "email" TEXT,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'PATIENT',
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "isBlocked" BOOLEAN NOT NULL DEFAULT false,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "receptionistId" TEXT,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "userProfiles" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "gender" "Gender" NOT NULL,
+    "dateOfBirth" TIMESTAMP(3) NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "emergencyContact" TEXT,
+    "relationWithEC" TEXT,
+    "presentAddress" TEXT,
+    "permanentAddress" TEXT,
+    "profilePicture" TEXT,
+    "bloodGroup" TEXT,
+
+    CONSTRAINT "userProfiles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "receptionists" (
+    "id" TEXT NOT NULL,
+    "userProfileId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "receptionists_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "patients" (
+    "id" TEXT NOT NULL,
+    "userProfileId" TEXT NOT NULL,
+    "createdById" TEXT,
+    "problems" TEXT[],
+    "diagnosis" TEXT,
+    "diagnosisBy" TEXT,
+    "diagnosisDate" TIMESTAMP(3),
+    "records" "RecordsType" NOT NULL DEFAULT 'PRESCRIPTION',
+    "referredBy" TEXT,
+    "visitedDoctorIds" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "patients_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Therapist" (
+    "id" TEXT NOT NULL,
+    "professionalId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Therapist_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "doctors" (
+    "id" TEXT NOT NULL,
+    "professionalId" TEXT NOT NULL,
+    "licenseNumber" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "doctors_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "admins" (
+    "id" TEXT NOT NULL,
+    "userProfileId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "admins_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "departments" (
+    "id" TEXT NOT NULL,
+    "name" "DepartmentType" NOT NULL DEFAULT 'OTHER',
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "departments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProfessionalProfile" (
+    "id" TEXT NOT NULL,
+    "userProfileId" TEXT NOT NULL,
+    "departmentId" TEXT NOT NULL,
+    "qualifications" TEXT[],
+    "experienceYears" INTEGER NOT NULL,
+    "specializations" TEXT[],
+    "consultationFee" INTEGER NOT NULL DEFAULT 0,
+    "availableOnline" BOOLEAN NOT NULL DEFAULT false,
+    "languagesSpoken" "LanguagePreference" NOT NULL DEFAULT 'BANGLA',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ProfessionalProfile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ScheduleAvailability" (
+    "id" TEXT NOT NULL,
+    "professionalId" TEXT NOT NULL,
+    "type" "ScheduleAvailabilityType" NOT NULL DEFAULT 'MONTHLY',
+    "dayOfWeek" INTEGER NOT NULL DEFAULT 0,
+    "specificDate" TIMESTAMP(3),
+    "fromDate" TIMESTAMP(3),
+    "toDate" TIMESTAMP(3),
+    "dayOfMonth" INTEGER,
+    "startTime" TEXT NOT NULL,
+    "endTime" TEXT NOT NULL,
+    "isAvailable" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ScheduleAvailability_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "appointments" (
+    "id" TEXT NOT NULL,
+    "patientId" TEXT NOT NULL,
+    "departmentId" TEXT NOT NULL,
+    "professionalId" TEXT NOT NULL,
+    "scheduleAvailabilityId" TEXT NOT NULL,
+    "creatorId" TEXT NOT NULL,
+    "appointmentDate" TIMESTAMP(3) NOT NULL,
+    "startTime" TEXT NOT NULL,
+    "endTime" TEXT NOT NULL,
+    "status" "AppointmentStatus" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "appointments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "appointmentNotes" (
+    "id" TEXT NOT NULL,
+    "appointmentId" TEXT NOT NULL,
+    "professionalId" TEXT NOT NULL,
+    "note" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "appointmentNotes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "appointmentFeedbacks" (
+    "id" TEXT NOT NULL,
+    "appointmentId" TEXT NOT NULL,
+    "patientId" TEXT NOT NULL,
+    "professionalId" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "comment" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "appointmentFeedbacks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "payments" (
+    "id" TEXT NOT NULL,
+    "appointmentId" TEXT NOT NULL,
+    "patientId" TEXT NOT NULL,
+    "professionalId" TEXT NOT NULL,
+    "departmentId" TEXT NOT NULL,
+    "transactionId" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL DEFAULT 0,
+    "status" "PaymentStatus" NOT NULL DEFAULT 'PENDING',
+    "method" "PaymentMethod" NOT NULL DEFAULT 'CASH',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "payments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_userKey_key" ON "users"("userKey");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "userProfiles_userId_key" ON "userProfiles"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "receptionists_userProfileId_key" ON "receptionists"("userProfileId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "patients_userProfileId_key" ON "patients"("userProfileId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Therapist_professionalId_key" ON "Therapist"("professionalId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "doctors_professionalId_key" ON "doctors"("professionalId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "admins_userProfileId_key" ON "admins"("userProfileId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "departments_name_key" ON "departments"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ProfessionalProfile_userProfileId_key" ON "ProfessionalProfile"("userProfileId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ScheduleAvailability_professionalId_specificDate_startTime__key" ON "ScheduleAvailability"("professionalId", "specificDate", "startTime", "endTime");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "appointments_professionalId_patientId_departmentId_appointm_key" ON "appointments"("professionalId", "patientId", "departmentId", "appointmentDate", "startTime", "endTime");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "appointmentNotes_appointmentId_key" ON "appointmentNotes"("appointmentId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "appointmentFeedbacks_appointmentId_key" ON "appointmentFeedbacks"("appointmentId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "payments_appointmentId_key" ON "payments"("appointmentId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "payments_transactionId_key" ON "payments"("transactionId");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_receptionistId_fkey" FOREIGN KEY ("receptionistId") REFERENCES "receptionists"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "userProfiles" ADD CONSTRAINT "userProfiles_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "receptionists" ADD CONSTRAINT "receptionists_userProfileId_fkey" FOREIGN KEY ("userProfileId") REFERENCES "userProfiles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "patients" ADD CONSTRAINT "patients_userProfileId_fkey" FOREIGN KEY ("userProfileId") REFERENCES "userProfiles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "patients" ADD CONSTRAINT "patients_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "receptionists"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Therapist" ADD CONSTRAINT "Therapist_professionalId_fkey" FOREIGN KEY ("professionalId") REFERENCES "ProfessionalProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "doctors" ADD CONSTRAINT "doctors_professionalId_fkey" FOREIGN KEY ("professionalId") REFERENCES "ProfessionalProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "admins" ADD CONSTRAINT "admins_userProfileId_fkey" FOREIGN KEY ("userProfileId") REFERENCES "userProfiles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProfessionalProfile" ADD CONSTRAINT "ProfessionalProfile_userProfileId_fkey" FOREIGN KEY ("userProfileId") REFERENCES "userProfiles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProfessionalProfile" ADD CONSTRAINT "ProfessionalProfile_departmentId_fkey" FOREIGN KEY ("departmentId") REFERENCES "departments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ScheduleAvailability" ADD CONSTRAINT "ScheduleAvailability_professionalId_fkey" FOREIGN KEY ("professionalId") REFERENCES "ProfessionalProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointments" ADD CONSTRAINT "appointments_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointments" ADD CONSTRAINT "appointments_departmentId_fkey" FOREIGN KEY ("departmentId") REFERENCES "departments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointments" ADD CONSTRAINT "appointments_professionalId_fkey" FOREIGN KEY ("professionalId") REFERENCES "ProfessionalProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointments" ADD CONSTRAINT "appointments_scheduleAvailabilityId_fkey" FOREIGN KEY ("scheduleAvailabilityId") REFERENCES "ScheduleAvailability"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointments" ADD CONSTRAINT "appointments_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "receptionists"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointmentNotes" ADD CONSTRAINT "appointmentNotes_professionalId_fkey" FOREIGN KEY ("professionalId") REFERENCES "ProfessionalProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointmentNotes" ADD CONSTRAINT "appointmentNotes_appointmentId_fkey" FOREIGN KEY ("appointmentId") REFERENCES "appointments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointmentFeedbacks" ADD CONSTRAINT "appointmentFeedbacks_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointmentFeedbacks" ADD CONSTRAINT "appointmentFeedbacks_professionalId_fkey" FOREIGN KEY ("professionalId") REFERENCES "ProfessionalProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointmentFeedbacks" ADD CONSTRAINT "appointmentFeedbacks_appointmentId_fkey" FOREIGN KEY ("appointmentId") REFERENCES "appointments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payments" ADD CONSTRAINT "payments_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payments" ADD CONSTRAINT "payments_professionalId_fkey" FOREIGN KEY ("professionalId") REFERENCES "ProfessionalProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payments" ADD CONSTRAINT "payments_departmentId_fkey" FOREIGN KEY ("departmentId") REFERENCES "departments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payments" ADD CONSTRAINT "payments_appointmentId_fkey" FOREIGN KEY ("appointmentId") REFERENCES "appointments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
